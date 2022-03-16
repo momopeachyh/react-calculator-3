5a63127d9627c4f7e1e88ae36164f5fc0592f990
Heidi <heidi.willden@me.com>
2022-03-16 17:19:03 -0500

    Addition function and styling

--- src/Addition.js
+++ src/Addition.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CalcContainer.css";
 
 function Addition() {
+  const [addAns, setAddAns] = useState("");
+
+  function calcSum(event) {
+    event.preventDefault();
+    let add1 = document.getElementById("add1").value;
+    let add2 = document.getElementById("add2").value;
+    let sum = Number(add1) + Number(add2);
+    setAddAns(sum);
+  }
+
   return (
     <div className="Addition">
       <input type="number" id="add1"></input>
       <span className="operator">+</span>
       <input type="number" id="add2"></input>
-      <button>=</button>
-      <input type="number" id="addAns"></input>
+      <button className="equal-button" onClick={calcSum}>
+        =
+      </button>
+      <input type="number" id="addAns" value={addAns}></input>
     </div>
   );
 }
