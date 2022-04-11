5640da71929aafb37edc8a1320a1106be8d154e1
Heidi <heidi.willden@me.com>
2022-04-11 17:02:50 -0500

    refactor

--- src/Addition.js
+++ src/Addition.js
@@ -2,21 +2,30 @@ import React, { useState } from "react";
 import "./CalcContainer.css";
 
 function Addition() {
+  let addNums = [];
   const [addAns, setAddAns] = useState("");
 
   function calcSum(event) {
     event.preventDefault();
-    let add1 = document.getElementById("add1").value;
-    let add2 = document.getElementById("add2").value;
-    let sum = Number(add1) + Number(add2);
-    setAddAns(sum);
+    // let add1 = document.getElementById("add1").value;
+    // let add2 = document.getElementById("add2").value;
+    // let sum = Number(add1) + Number(add2);
+    // setAddAns(sum);
+    console.log("button clicked");
+  }
+
+  function getValue(event) {
+    event.preventDefault();
+    let add1 = event.target.value;
+    addNums.push(add1);
+    console.log(addNums);
   }
 
   return (
     <div className="Addition">
-      <input type="number" id="add1"></input>
+      <input type="number" id="add1" onInput={getValue}></input>
       <span className="operator">+</span>
-      <input type="number" id="add2"></input>
+      <input type="number" id="add2" onInput={getValue}></input>
       <button className="equal-button" onClick={calcSum}>
         =
       </button>
